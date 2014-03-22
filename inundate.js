d502fbb7e27f82e89bfce1cb6141bffed615031f
Nat Wilson <njwilson23@gmail.com>
2014-03-22 17:48:11 -0400

    adjust to changes in planned API

--- inundate.js
+++ inundate.js
@@ -1,10 +1,15 @@
+'use strict;'
 /*
 Implement a map of coastal areas (in North America, for now) that visualizes the potential coastline changes due to sea level rise.
 */
 
 var bounds = [-130, -70, 20, 60];
 var map = Scrollmap(bounds);
+var canvas = document.getElementById("map");
+var ctx = canvas.getContext('2d');
 
-map.addCoast('data/coast/');         // this will be the modern coastline data
-map.addPolygon('data/slr100/');      // when the app starts, a feature representing 1m of SLR will be shown
+map.readIndex('data/coast/')    // this will be the modern coastline data
+   .readIndex('data/slr100/')   // a feature representing 1m of SLR
+   .update()
+   .drawTo(ctx);
 
