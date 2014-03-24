88da3c2884540d0d133edb04ea5dc625fa3f9940
Nat Wilson <njwilson23@gmail.com>
2014-03-23 23:15:41 -0400

    basic working app; shows map
    the app can now read the index file, use it to download the correct tile
    topojson, and display the data using SVG

--- scrollmap.js
+++ scrollmap.js
@@ -4,7 +4,7 @@ This implements a scrollable map drawn using d3 and the <canvas> element. This
 is a major component of inundate.js.
 */
 
-function Scrollmap(bounds) {
+function Scrollmap(svg, bounds) {
 
   if (bounds[1] < bounds[0]) {
     throw "x1 must be greater than x0";
@@ -12,17 +12,19 @@ function Scrollmap(bounds) {
     throw "x3 must be greater than x2";
   }
 
+  this.svg = svg;
   this.xy = [0.5*(bounds[0] + bounds[1]), 0.5*(bounds[2] + bounds[3])];
   this.aspect = (bounds[1] - bounds[0]) / (bounds[3] - bounds[2])
   this.scale = bounds[1] - bounds[0];
   this.indices = [];  // this will a list of indices to all the feature to display
-  this.tiles = {};  // this will be a hash map of the currently "owned" tiles
+  this.tiles = [];    // this will be a hash map of the currently "owned" tiles
 }
 
 Scrollmap.prototype.readIndex = function (addr) {
   var sm = this;
   d3.json(addr, function (err, data) {
     sm.indices.push(data);
+    sm.update();
   })
   return this;
 }
@@ -38,6 +40,7 @@ Scrollmap.prototype._tilesAt = function (xy) {
                 xy[1] - 0.5*dy,
                 xy[1] + 0.5*dy];
 
+  // search the indices for tiles in view
   // the following loop leaves room for optimization
   var tiles = [];
   for (var ii=0; ii < this.indices.length; ii++) {
@@ -48,6 +51,8 @@ Scrollmap.prototype._tilesAt = function (xy) {
       var b = tile.bounds;
       if (((b[1] > bounds[0]) || (b[0] < bounds[1])) &&
           ((b[3] > bounds[2]) || (b[2] < bounds[3]))) {
+        tile.fullAddr = index.topAddr + tile.addr;
+        tile.class = index.class;
         tiles.push(tile);
       }
     }
@@ -60,33 +65,48 @@ Scrollmap.prototype.update = function () {
 
   // add tiles missing from local
   for (var i=0; i<tiles.length; i++) {
-    if (tiles[i] in this.tiles === false) {
+    if (this.tiles.indexOf(tiles[i]) === -1) {
       this.tiles.push(tiles[i]);
+      this.loadTile(tiles[i]);
     }
   }
 
   // remove local tiles not in the new list
   for (var i=0; i<this.tiles.length; i++) {
-    if (this.tiles[i] in tiles === false) {
+    if (tiles.indexOf(this.tiles[i]) === -1) {
       this.tiles.pop(i);
+      // remove points from svg
+      console.log("Not implemented - cleanup point from SVG");
     }
   }
 
   return this;
 }
 
-Scrollmap.prototype.loadTile = function (addr) {
+Scrollmap.prototype.loadTile = function (tile) {
   var sm = this;
-  d3.json(addr, function (err, data) {
+  d3.json(tile.fullAddr, function (err, data) {
     // check for errors?
+    data.class = tile.class;
     sm.tiles.push(data);
+    sm.draw(data);
   })
   return this;
 }
 
+Scrollmap.prototype.draw = function (tile) {
+  this.svg.append("path")
+    .datum(topojson.feature(tile, tile.objects.ne_10m_coastline))
+    .attr("class", tile.class)
+    .attr("d", d3.geo.path().projection(d3.geo.mercator()));
+}
+
 Scrollmap.prototype.drawTo = function (svg) {
-  // a bunch of messy draw logic
-  // using d3 or direct to canvas?
-  console.log('draw');
+  for (var i=0; i<this.tiles.length; i++) {
+    var tile = this.tiles[i]
+    svg.append("path")
+      .datum(topojson.feature(tile, tile.data.ne_10m_coastline))
+      .attr("d", d3.geo.path().projection(d3.geo.mercator()));
+  }
 }
 
